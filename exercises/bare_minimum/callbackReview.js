8a9ad82d604ee986716b412ba3c13434004e8ab4
Daisy Ding <xd222@cornell.edu>
2022-08-01 19:30:50 -0700

    Complete bare_minimum/callbackReview.js exercises

--- exercises/bare_minimum/callbackReview.js
+++ exercises/bare_minimum/callbackReview.js
@@ -6,13 +6,26 @@ var fs = require('fs');
 var request = require('needle');
 
 // This function should retrieve the first line of the file at `filePath`
-var pluckFirstLineFromFile = function (filePath) {
-  // TODO
+var pluckFirstLineFromFile = function (filePath, cb) {
+  fs.readFile(filePath, 'utf8', function (err, content) {
+    if (err) {
+      cb(err);
+    } else {
+      var storage = content.split('\n');
+      cb(err, storage[0]);
+    }
+  });
 };
 
 // This function should retrieve the status code of a GET request to `url`
-var getStatusCode = function (url) {
-  // TODO
+var getStatusCode = function (url, cb) {
+  request.get(url, function(err, content) {
+    if (err) {
+      cb(err);
+    } else {
+      cb(err, content.statusCode);
+    }
+  });
 };
 
 // Export these functions so we can test them and reuse them in later exercises
